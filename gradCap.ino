72fe78a6f4fdb1e1c60f22be6b6b551363bcb2bd
fahadmirza <fahadmirza80@yahoo.com>
2016-09-18 00:38:49 -0500

    Added second animation 'glowingCircles'

--- gradCap.ino
+++ gradCap.ino
@@ -1,9 +1,20 @@
-
+/**
+  ******************************************************************************
+  * @file       main.c 
+  * @author     Fahad Mirza
+  * @version    V1.0
+  * @date       09-July-2016
+  * @modified   11-September-2016
+  * @brief      Main program body
+  ******************************************************************************
+  */
 
 
 #include <Adafruit_NeoPixel.h>
-#define PIN 6
+#define CONTROL_PIN 6
 
+
+/****************************** Global Variables ******************************/
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -12,130 +23,73 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(40, CONTROL_PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
-uint32_t greenShade[26];
-int16_t x;
-int16_t i;
+
+uint32_t greenShade[15];  // For 15 different preset shades
+
+/* Ignore the 'led' variable. I wanted to start all the animation from a specific 
+ * pixel for the 16 LED ring. If you are a perfectionist freak like me I guess 
+ * then you will evemtually figure out what you want and what to do. But for now
+ * just ignore.
+ */
 uint16_t led[16]={26,25,24,39,38,37,36,35,34,33,32,31,30,29,28,27};
+
+
+/****************************** Setup function ******************************/
 void setup() 
 {
-//  Serial.begin(115200);
+  //Serial.begin(115200);
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(60);
-  int x=0;
-  for(int j=0; j<=150;j=j+10)
+  strip.show();             // Initialize all pixels to 'off'
+  strip.setBrightness(60);  // Limit overall brightness to 60% in order to lengethen the battery life 
+  
+  // Creating the green shades started from Zero to 150 with a stepping value of 10
+  int index=0;
+  for(int i = 0; i <= 150; i = i+10)
   {
-      greenShade[x++]= strip.Color(0, j, 0);
+      greenShade[index++]= strip.Color(0, i, 0);
   }
+  
   pinMode(13, OUTPUT);
-  digitalWrite(13,0);
+  digitalWrite(13,0);   // For debug. This will indicate the board indeed is working!
 }
+
 
 void loop() 
 {
-  // Some example procedures showing how to display to the pixels:
+  int16_t x; // 'for' loop variables
+  int16_t i;
+
+  
+/************ Animation 1: Fill the Pixels one after the other ************/
   colorWipe(strip.Color(0, 150, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 0), 50); // off
+  colorWipe(strip.Color(0, 0, 0), 50);   // off
   delay(1000);
-  for(int k=0; k<2;k++)
-    {
-        for(x=0; x<=7;x++)
-        {
-          for (i=0; i <= 23; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
-        for(x=8; x<=15;x++)
-        {
-          for (i=0; i <= 23; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
-          }
-          for (i=24; i <= 39; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x-8]);    //turn every third pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
-        for(x=8; x<=15;x++)
-        {
-          for (i=24; i <= 39; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every third pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
 
 
 
-        for(x=15; x>=8;x--)
-        {
-          for (i=24; i <= 39; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every third pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
-
-        
-        for(x=7; x>=0;x--)
-        {
-          for (i=0; i < 24; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x+8]);    //turn every  pixel on
-          }
-          for (i=24; i < 40; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every third pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
-        for(x=7; x>=0;x--)
-        {
-          for (i=0; i < 24; i++) 
-          {
-            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
-          }
-          strip.show();
-    
-          delay(50);
-        }
-    }
+/****************************** Animation 2 ******************************/
+  glowingCircles(2,50)
   delay(1000);
-//colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
-  // Send a theater pixel chase in...
   
 
-    for(i=0; i<151;i=i+10)
-    {
+  for(i=0; i<151;i=i+10)
+  {
       theaterChase(strip.Color(0, i, 0), 50); // GREEN
-    }
-    for(i=150; i>=0;i=i-10)
-    {
-//      Serial.println(i);
-      theaterChase(strip.Color(0, i, 0), 50); // GREEN
-    }
-    delay(1000);
-    for(int k=0; k<2;k++)
-    {
+  }
+  for(i=150; i>=0;i=i-10)
+  {
+    //Serial.println(i);
+    theaterChase(strip.Color(0, i, 0), 50); // GREEN
+  }
+  delay(1000);
+  for(int k=0; k<2;k++)
+  {
         for(int j=0; j<151;j=j+10)
         {
           for (i=0; i < strip.numPixels(); i++) 
@@ -156,34 +110,142 @@ void loop()
           strip.show();
           delay(50);
         }
-    }
-    delay(1000);
+  }
+  delay(1000);
 
     
-
-
-//  rainbow(20);
-//  rainbowCycle(20);
-//  theaterChaseRainbow(50);
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait)
+
+/*
+ * @brief  Fill the Pixels(LEDs) one after the other with a color
+ * @param  color_: 32bit color type, 
+ *         wait  : delay in ms
+ * @retval none
+ */
+void colorWipe(uint32_t color_, uint8_t wait)
 {
   for(uint16_t i=0; i<24; i++) 
   {
-    strip.setPixelColor(i, c);
+    strip.setPixelColor(i, color_);
     strip.show();
     delay(wait);
   }
+  
+  // If you are ignoring 'led' variable then use the commented out code and comment out the other 'for' loop
+ 
+  // for(uint16_t i=24; i<40; i++) 
+  // {
+  //    strip.setPixelColor(i, color_);
+  //    strip.show();
+  //    delay(wait);
+  // }  
+  
   for(uint16_t i=0; i<16; i++) 
   {
-    strip.setPixelColor(led[i], c);
+    strip.setPixelColor(led[i], color_);
     strip.show();
     delay(wait);
   }
 }
 
+
+/*
+ * @brief  Glow the rings one after the other
+ * @param  times: how many times you want to play the animation, 
+ *         wait : delay in ms
+ * @retval none
+ */
+void glowingCircles(uint8_t times, uint8_t wait)
+{
+  uint8_t x;
+  uint8_t i;
+  for(uint8_t k = 0; k < times; k++)
+  {
+      // Start to glow the outer circle half way
+      for(x=0; x<=7;x++)
+      {
+          for (i=0; i <= 23; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
+          }
+          strip.show();
+          delay(wait);
+      }
+
+      // Now complete full cycle for the outer circle and start to glow inner cicle half way
+      for(x=8; x<=15;x++)
+      {
+          for (i=0; i <= 23; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
+          }
+          for (i=24; i <= 39; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x-8]);
+          }
+          strip.show();    
+          delay(wait);
+      }  
+
+      // Now complete full cycle for the inner circle
+      for(x=8; x<=15;x++)
+      {
+          for (i=24; i <= 39; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]);    
+          }
+          strip.show();
+          delay(wait);
+      }
+
+      // Now reverse the whole thing
+      for(x=15; x>=8;x--)
+      {
+          for (i=24; i <= 39; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]); 
+          }
+          strip.show();
+          delay(wait);
+      }
+      for(x=7; x>=0;x--)
+      {
+          for (i=0; i < 24; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x+8]);    //turn every  pixel on
+          }
+          for (i=24; i < 40; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]);
+          }
+          strip.show();
+          delay(wait);
+      }
+      for(x=7; x>=0;x--)
+      {
+          for (i=0; i < 24; i++) 
+          {
+            strip.setPixelColor(i, greenShade[x]);    //turn every  pixel on
+          }
+          strip.show();     
+          delay(wait);
+      }
+  }
+}
+
+
+
+
+
+
+
+/*
+ * @brief  Fill the Pixels(LEDs) one after the other with a color
+ * @param  color_: 32bit color type, 
+ *         wait  : delay in ms
+ * @retval none
+ */
 void rainbow(uint8_t wait) 
 {
   uint16_t i, j;
